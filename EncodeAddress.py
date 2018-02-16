d2508be5577dacd0a670446c4c333cb1f3eb7930
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2018-02-15 20:07:39 -0800

    Add octal and binary functionality to the address conversion.

--- EncodeAddress.py
+++ EncodeAddress.py
@@ -69,8 +69,67 @@ class EncodeAddress:
             print "Either no connection or invalid DNS"
             return -1
 
+    #inspiration
+    #https://superuser.com/questions/75930/open-websites-using-binary-ip-address
+    def convert_to_octal(self,domain_name):
+        """
+        Converts the domain name into an octal ip address
+        Args:
+            domain_name(string): the DNS or ip address to be converted
+        Returns:
+            address(string): the octal IP address
+        """
+
+        #parses the input
+        DNS = self.fix_address(domain_name)
+        #gets the IP of the domain_name, if it's a domain name
+        ip = self.get_ip(DNS)
+        ip = ip.split(".")
+        ip_list = list()
+        for spot in ip:
+            ip_list.append(str((oct(int(spot)).zfill(4))))
+
+        print ip_list
+        ip_address = ""
+        for bi in ip_list:
+            ip_address += bi
+            ip_address+="."
+        return "http://" + ip_address[:-1]
+
+    def convert_to_binary(self,domain_name):
+        """
+        Converts the domain name into a binary ip address
+        Args:
+            domain_name(string): the DNS or ip address to be converted
+        Returns:
+            address(string): the binary IP address
+        """
+
+        #parses the input
+        DNS = self.fix_address(domain_name)
+        #gets the IP of the domain_name, if it's a domain name
+        ip = self.get_ip(DNS)
+        ip = ip.split(".")
+        ip_list = list()
+        for spot in ip:
+            ip_list.append(str((bin(int(spot))[2:].zfill(8))))
+
+        print ip_list
+        ip_address = ""
+        for bi in ip_list:
+            ip_address += bi
+            ip_address+="."
+
+        return "http://" + ip_address[:-1]
+
+    def convert_offset(self,domain_name):
+        """
+        Want to be able to specify which to convert to what!
+        """
+        pass
 
 if __name__ == '__main__':
     E = EncodeAddress()
-    print "Decimal IP address for facebook.com: ",
-    print E.convert_to_decimal("facebook.com")
+    print E.convert_to_octal("google.com")
+    #print "Decimal IP address for facebook.com: ",
+    #print E.convert_to_decimal("209.85.129.99")
